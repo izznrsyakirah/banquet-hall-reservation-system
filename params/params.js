77a983ef2b2407dbdbab7462b1a5b6740c6e3bbe
Slash Hj Tapah <imuqri31@gmail.com>
2023-07-21 21:06:28 +0800

    update atlas config

--- params/params.js
+++ params/params.js
@@ -1,4 +1,4 @@
-const DATABASECONNECTION = "mongodb://0.0.0.0:27017/hall"
+const DATABASECONNECTION = "mongodb+srv://iznnur:OgP2Ce8IJ9XQL2mh@cluster0.grwdp0a.mongodb.net/"
 
 module.exports = {
     DATABASECONNECTION
