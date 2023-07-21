fd4010e121ed063ab4810bef6679db65395bdb28
Slash Hj Tapah <imuqri31@gmail.com>
2023-07-21 21:29:01 +0800

    Update index working atlas

--- index.js
+++ index.js
@@ -72,6 +72,8 @@ app.set("port", process.env.PORT || 5000);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 //app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(session({
